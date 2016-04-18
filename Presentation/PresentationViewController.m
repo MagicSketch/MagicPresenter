edd06ae9bcd67355e57b8945697c157160edcbc4
James Tang <j@jamztang.com>
2016-04-18 12:36:57 +0800

    Use Keyboard to Navigate through Artboards

--- Presentation/PresentationViewController.m
+++ Presentation/PresentationViewController.m
@@ -12,7 +12,9 @@
 
 @property (weak) IBOutlet NSImageView *imageView;
 @property (nonatomic, strong) NSArray *slides;
-@property (nonatomic, assign) NSUInteger index;
+@property (nonatomic, assign) NSInteger index;
+@property (nonatomic, strong) id monitor;
+@property (nonatomic, assign) CGSize size;
 
 @end
 
@@ -22,19 +24,76 @@
     [super viewDidLoad];
     // Do view setup here.
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self.view.window toggleFullScreen:nil];
-    });
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        [self.view.window toggleFullScreen:nil];
+//    });
+
+    __weak __typeof (self) weakSelf = self;
+    _monitor = [NSEvent addLocalMonitorForEventsMatchingMask:NSKeyDownMask
+                                                     handler:^NSEvent * _Nullable(NSEvent * _Nonnull event) {
+                                                         [weakSelf keyDown:event];
+                                                         return event;
+                                                     }];
+
+    self.view.wantsLayer = YES;
+    self.view.layer.backgroundColor = [NSColor blackColor].CGColor;
+}
+
+- (void)viewWillAppear {
+    [super viewWillAppear];
+    [self centerWindow];
+}
+
+- (void)dealloc {
+    [NSEvent removeMonitor:_monitor];
 }
 
 - (void)reloadData {
-    self.imageView.image = [_slides[_index] image];
+    NSImage *image = [_slides[_index] image];
+    self.imageView.image = image;
+    self.size = image.size;
 }
 
 - (void)setSlides:(NSArray *)slides atIndex:(NSUInteger)index {
     self.slides = slides;
     self.index = index;
     [self reloadData];
+}
+
+- (void)centerWindow {
+    NSWindow *window = self.view.window;
+    CGFloat xPos = NSWidth([[window screen] frame])/2 - _size.width/2;
+    CGFloat yPos = NSHeight([[window screen] frame])/2 - _size.height/2;
+    [window setFrame:NSMakeRect(xPos, yPos, _size.width, _size.height) display:YES];
+}
+
+- (void)keyDown:(NSEvent *)theEvent {
+    switch (theEvent.keyCode) {
+        case 126: // Up
+        case 123: // Left
+            [self goPreviousPage];
+            break;
+        case 125: // Down
+        case 124: // Right
+            [self goNextPage];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)goPreviousPage {
+    if (_index + 1 < _slides.count) {
+        _index++;
+        [self reloadData];
+    }
+}
+
+- (void)goNextPage {
+    if (_index - 1 >= 0) {
+        _index--;
+        [self reloadData];
+    }
 }
 
 @end
