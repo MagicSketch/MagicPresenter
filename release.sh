ab2cd9dafe53f58e575bdf4481dc8ec1d4117260
Mick <mick.mak@busicat.com>
2016-05-11 00:07:08 +0800

    release script fix.

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ xctool -scheme MagicPresenter -configuration Release > Logs/1-xcode.log
 
 echo "Creating MagicPresenter.zip"
 
-zip MagicPresenter.zip *.sketchplugin coscript test.sh RunTests.js README.md MagicPresenter.sketch install.sh > Logs/2-zip.log
+zip -r MagicPresenter.zip *.sketchplugin coscript test.sh RunTests.js README.md MagicPresenter.sketch install.sh > Logs/2-zip.log
 
 
 # git tag -a "v1.0" -m "Initial Public Release"
