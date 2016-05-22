c7413f61b56ad1f37fbef9772d9c5b815f0f1d44
James Tang <j@jamztang.com>
2016-05-23 03:31:47 +0800

    Add release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     home: process.env.HOME,
+    version: "v0.3",
     xcode: {
       options: {
         project: 'MagicPresenter.xcodeproj',
@@ -41,10 +42,41 @@ module.exports = function(grunt) {
           'appdmg release.json MagicPresenter.dmg'
         ].join(' && ')
       },
+      tag: {
+        command: [
+          'git tag -a <%= version %> -m "<%= version %>"',
+          'git push origin <%= version %>',
+        ].join(' && ')
+      },
+    },
+    gh_release: {
+      options: {
+        // Credentials, required
+        token: process.env.GITHUB_TOKEN,
+        owner: 'MagicSketch',
+        repo: 'MagicPresenter'
+      },
+      release: {
+        // Release input
+        // All options are optional, details: http://developer.github.com/v3/repos/releases/#input-1
+        tag_name: '<%= version %>',
+        // target_commitish: 'master',
+        name: '<%= version %>',
+        body: 'Description of the release.',
+        draft: false,
+        prerelease: true,
+        asset: {
+          // Upload a release asset if needed
+          name: 'MagicPresenter.dmg',
+          file: 'MagicPresenter.dmg',
+          'Content-Type': 'application/x-apple-diskimage' // Common media types: http://en.wikipedia.org/wiki/Internet_media_type#List_of_common_media_types
+        }
+      }
     },
   });
 
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-gh-release');
 
   grunt.registerTask('kill', ['shell:kill']);
   grunt.registerTask('test', ['shell:test']);
@@ -52,5 +84,7 @@ module.exports = function(grunt) {
   grunt.registerTask('build', ['shell:build']);
   grunt.registerTask('archive', ['shell:archive']);
   grunt.registerTask('default', ['shell:build', 'shell:install', 'shell:archive']);
+  grunt.registerTask('upload', ['shell:tag', 'gh_release']);
+  grunt.registerTask('release', ['shell:build', 'shell:archive', 'shell:tag', 'gh_release']);
 
 };
